59c29f032f099fcf71d5f81a7edf3cd009740fbe
Olha Zhokhova <zhokhova.olha@gmail.com>
2022-02-12 13:58:39 +0200

    fix basename

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename="https://github.com/olhazhokhova/goit-react-hw-05-movies">
+    <BrowserRouter basename="https://olhazhokhova.github.io/">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
